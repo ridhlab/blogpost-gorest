c744a291114487d48bd225af939e87c150a3c4f3
ridhlab <mohridwan1707@gmail.com>
2024-02-01 15:37:58 +0700

    remove console.log

--- src/api/posts/index.ts
+++ src/api/posts/index.ts
@@ -6,10 +6,6 @@ import { IPostResponseIndex } from "@/interfaces/responses/post";
 export async function getPostIndex(
     query?: Record<string, string>
 ): Promise<IPostResponseIndex> {
-    console.log(
-        query,
-        routeWithParams(GOREST_ENDPOINT.POSTS.INDEX, query ?? {})
-    );
     const response = await fetch(
         routeWithParams(GOREST_ENDPOINT.POSTS.INDEX, query ?? {})
     );
