1b09dff5a6221698dae08a08f1769f77f00fac1b
ridhlab <mohridwan1707@gmail.com>
2024-02-04 10:44:21 +0700

    fix(tag): adjust bg for dark theme

--- src/components/shared/tag/tag.tsx
+++ src/components/shared/tag/tag.tsx
@@ -10,11 +10,11 @@ export default function Tag({ label, color = "primary" }: IProps) {
     const baseClassnames = "text-[10px] rounded p-1";
     const colorClassnames = (() => {
         if (color === "primary")
-            return "border border-blue-500 text-blue-500 bg-blue-50";
+            return "border border-blue-500 text-blue-500 bg-blue-50 dark:bg-[rgba(96,153,227,0.26)]";
         if (color === "danger")
-            return "border border-red-500 text-red-500 bg-red-50";
+            return "border border-red-500 text-red-500 bg-red-50 dark:bg-[rgba(207,84,84,0.28)]";
         if (color === "success")
-            return "border border-green-500 text-green-500 bg-green-50";
+            return "border border-green-500 text-green-500 bg-green-50 dark:bg-[rgba(73,183,106,0.28)]";
     })();
     const mergedClassnames = [baseClassnames, colorClassnames];
     return <span className={twMerge(clsx(mergedClassnames))}>{label}</span>;
